36ab5117dd30612a0be74cf53d78077e3c45cced
sgatlewa <gatlewar.santa544@gmail.com>
2015-08-03 05:56:31 -0700

    1-checkin

---
+++ src/com/dmart/service/BarCodeEnquire.java
@@ -0,0 +1,21 @@
+package com.dmart.service;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+@Path("/barcode")
+public class BarCodeEnquire {
+	
+	@GET
+	@Path("/{param}")
+	public Response getMsg(@PathParam("param") String msg) {
+ 
+		String output = "Jersey say : " + msg;
+ 
+		return Response.status(200).entity(output).build();
+ 
+	}
+
+}
